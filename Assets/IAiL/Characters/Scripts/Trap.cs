ccd0338f80d32de28a181cb8c60d0fe57413e340
IAiL <qkrtjdqls1028@gmail.com>
2022-02-27 10:56:57 +0900

    trap cs

---
+++ Assets/IAiL/Characters/Scripts/Trap.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trap : MonoBehaviour
+{
+
+    Animator anim;
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            anim.SetBool("Catchingdaramjwi", true);
+        }
+    }
+}
